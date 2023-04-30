ee76350d8a2bf30d714edaba5333c71bdfbc6a86
shadowhunter100 <shadowhunter100@users.noreply.github.com>
2023-04-30 11:47:11 -0700

    Revert "the "?" name issue should be fixed... apparently the names table was being referenced before it was ready."
    This reverts commit 84fdc6b566e1521146eb4203c71c89d9c87ff2d3.

--- mods/SuperbSurvivorsContinued/media/lua/client/1_Dialogue/SuperSurvivorNames.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/1_Dialogue/SuperSurvivorNames.lua
@@ -1,23 +1,22 @@
 ---  Below is hundreds of girl names and boy names. you dont need to translate them if you don't want to. translate as in, replace them with common names in area of language or change to your phonetic alphabeht. if you do. know that there does not need to be this many. you could just do like 50 of each.
+ 
 local function getGirlNames()
-  local girlNamesCount = 150;
+  local girlNamesCount = 500
 
-  local names = {};
-
+  local names = {}
   for i = 1, girlNamesCount, 1 do
-    names[i] = getName("Name:" .. tostring(i));
+    names[i] = getName("Names" .. tostring(i))
   end
 
   return names
 end
 
 local function getBoyNames()
-  local boyNamesCount = 150;
+  local boyNamesCount = 160
 
-  local names = {};
-
+  local names = {}
   for i = 1, boyNamesCount, 1 do
-    names[i] = getName("Name:" .. tostring(i));
+    names[i] = getName("Namb" .. tostring(i))
   end
 
   return names
@@ -26,19 +25,22 @@ end
 ---@alias	gender
 ---| "GirlNames"
 ---| "BoyNames"
-SurvivorNameTable = {
-  ["GirlNames"] = getGirlNames(),
-  ["BoyNames"] = getBoyNames()
-};
 
 --- gets a random survivor name based on gender
 ---@param key gender key of the name
 ---@return string a random survivor name
-function GetRandomName(key)
-  if (not SurvivorNameTable[key]) then
-    return "Error: No Name";
-  end
-
-  local result = ZombRand(1, #SurvivorNameTable[key]);
-  return tostring(SurvivorNameTable[key][result]);
+function getRandomName(key)
+	if(not SurvivorNameTable[key]) then 
+		return "?" 
+	end
+	
+	local result = ZombRand(1,#SurvivorNameTable[key]);
+	return tostring(SurvivorNameTable[key][result]);
 end
+
+if not SurvivorNameTable then 
+  SurvivorNameTable = {} 
+end
+
+SurvivorNameTable["GirlNames"] = getGirlNames()
+SurvivorNameTable["BoyNames"] = getBoyNames()
