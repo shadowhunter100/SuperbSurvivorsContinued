ec37a947adf2c78bd92886ea8bcc455589d7947b
shadowhunter100 <shadowhunter100@users.noreply.github.com>
2023-05-01 11:29:57 -0700

    Moved HairColors into global variables

--- mods/SuperbSurvivorsContinued/media/lua/client/09_Spawning/SuperSurvivorPresetSpawns.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/09_Spawning/SuperSurvivorPresetSpawns.lua
@@ -1688,7 +1688,6 @@ PresetSpawns[#PresetSpawns + 1] = {
 	isHostile = false
 };
 
-
 CannedFoods = { "TinnedBeans", "CannedBolognese", "CannedCarrots2", "CannedChili", "CannedCorn", "CannedCornedBeef",
 	"CannedMushroomSoup", "CannedPeas", "CannedPotato2", "CannedSardines", "TinnedSoup", "CannedTomato2" };
 
@@ -1709,19 +1708,6 @@ SurvivorRoutes = {
 	},
 
 }
-
-HairColors = {}
-HairColors["white"] = ImmutableColor.new(0.75, 0.74, 0.72)
-HairColors["grey"] = ImmutableColor.new(0.48, 0.47, 0.44)
-HairColors["blond"] = ImmutableColor.new(0.82, 0.82, 0.39)
-HairColors["sand"] = ImmutableColor.new(0.86, 0.78, 0.66)
-HairColors["hazel"] = ImmutableColor.new(0.61, 0.50, 0.34)
-HairColors["brown"] = ImmutableColor.new(0.62, 0.42, 0.17)
-HairColors["red"] = ImmutableColor.new(0.58, 0.25, 0.25)
-HairColors["pink"] = ImmutableColor.new(0.59, 0.39, 0.55)
-HairColors["purple"] = ImmutableColor.new(0.47, 0.43, 0.59)
-HairColors["blue"] = ImmutableColor.new(0.39, 0.47, 0.59)
-HairColors["black"] = ImmutableColor.new(0.10, 0.08, 0.09)
 
 
 function SuperSurvivorPresetSpawnThis(PresetSpawn)
