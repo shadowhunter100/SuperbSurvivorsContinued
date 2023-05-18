69b96257eec324c4d02955341ff2ca1cab9b0dc2
shadowhunter100 <shadowhunter100@users.noreply.github.com>
2023-05-18 11:18:42 -0700

    Updated formatting

--- mods/SuperbSurvivorsContinued/media/lua/client/01_SuperSurvivorsOptions/SuperSurvivorsInitOptions.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/01_SuperSurvivorsOptions/SuperSurvivorsInitOptions.lua
@@ -2,15 +2,14 @@
 	thanks and some credit to Fenris_Wolf from ORGM mod for creating this hotkeys file!
 	I just tweaked it to use my own key bindings! and added support for settings too ~Nolan
 	Cows: I've added the Fenris_Wolf in the github credits.
---]]
---[[
+	
 	WIP - Cows: This file is replacing the old "SuperSurvivorSettings.lua" file and "SuperSurvivorOptions" variables...
 	WIP - Cows: Need to review for issues, and separating the functions into smaller blocks for readability and easier maintenance.
 --]]
 local isLocalLoggingEnabled = false;
 
 -- NPC Spawning
-NpcSpawnChance = 50;       -- WIP - Cows: NpcSpawnChance (formerly "AlternativeSpawning") is used when the player is in the current map area
+NpcSpawnChance = 50;      -- WIP - Cows: NpcSpawnChance (formerly "AlternativeSpawning") is used when the player is in the current map area
 HostileSpawnRateBase = 1; -- Cows: Chance that NPCs will be hostile initially on spawn
 HostileSpawnRateMax = 17; -- WIP - Cows: Chance the NPCs will be hostile on spawn as time pass, capped at this value... need to test and verify.
 IsWifeSpawn = true;       -- Cows: true to spawn wife / 1st follower, false to not spawn wife / 1st follower
@@ -21,8 +20,8 @@ Max_Group_Size = 4;       -- WIP - PlaceHolder - Cows: Max number of members in 
 Min_Group_Size = 1;       -- WIP - PlaceHolder - Cows: Min number of members in a group
 
 -- Raiders, Always hostile
-Limit_Raiders_Groups = 4; -- WIP - PlaceHolder - Cows: Max raider groups, independent of npc groups.
-Limit_Raiders_Spawn = 8;  -- WIP - PlaceHolder - Cows: Max raiders spaws, independent of npcs spawns.
+Limit_Raiders_Groups = 4;          -- WIP - PlaceHolder - Cows: Max raider groups, independent of npc groups.
+Limit_Raiders_Spawn = 8;           -- WIP - PlaceHolder - Cows: Max raiders spaws, independent of npcs spawns.
 RaidersSpawnFrequencyByHours = 24; -- WIP - Cows: Spawn frequency in this example is to guarantee a raiders spawn once every 24 hours
 RaidersStartAfterHours = 0;        -- WIP - Cows: Supposedly determines when raider can start spawning after set hours.
 RaidersSpawnChance = 50;           -- WIP - Cows: this is still in testing... apparently used to check for a raiders spawn every 10 minutes...
@@ -38,7 +37,6 @@ WepSpawnRateMelee = 100;      -- Cows: Melee Weapon Spawn rate... should be set 
 
 -- NPC Behaviors
 CanIdleChat = false;            -- Cows: true to allow npcs to speak while idle
--- old comment - Update: Don't try to turn gfollowdistance into a variable from what it equals to. I made followtask add what it needs to add on its own.
 GFollowDistance = 5;            -- WIP - Cows: need to verify the old comment in testing...
 PanicDistance = 21;             -- WIP - Cows: Value is used in FleeFromHereTask()... but Fleeing needs to be reworked eventually...
 SuperSurvivorBravery = 6;       -- WIP - Cows: Bravery needs to be reworked... because it is literally useless at the moment.
@@ -56,7 +54,6 @@ SurvivorNeedsFoodWater = false; -- Cows: true to activate npc survivor's hunger 
 	Assuming higher = friendlier, why even bother with the calculation?
 --]]
 SurvivorFriendliness = 10;
-
 
 -- Player Related
 IsPlayerBaseSafe = true; -- WIP - Cows: true to prevent NPCs from claiming or visiting the player base... this needs to be tested and verified.
