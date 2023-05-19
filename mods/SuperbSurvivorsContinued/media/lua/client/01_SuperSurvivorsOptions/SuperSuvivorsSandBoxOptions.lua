bdd9c8fba07ff03001070ad5e6759b19f2906489
shadowhunter100 <shadowhunter100@users.noreply.github.com>
2023-05-18 18:35:09 -0700

    Working Sandbox Options added.

---
+++ mods/SuperbSurvivorsContinued/media/lua/client/01_SuperSurvivorsOptions/SuperSuvivorsSandBoxOptions.lua
@@ -0,0 +1,43 @@
+-- Group Size applies to both npcs and raiders.
+Max_Group_Size = SandboxVars.SuperbSurvivorsContinued.Max_Group_Size;
+-- Min_Group_Size = SandboxVars.SuperbSurvivorsContinued.Min_Group_Size; -- Cows: Doesn't need to be set, min group size is always 1.
+
+-- NPC Spawning
+Limit_Npc_Groups = SandboxVars.SuperbSurvivorsContinued.Limit_Npc_Groups;
+Limit_Npcs_Spawn = SandboxVars.SuperbSurvivorsContinued.Limit_Npcs_Spawn;
+IsWifeSpawn = SandboxVars.SuperbSurvivorsContinued.IsWifeSpawn;
+NoPresetSpawn = SandboxVars.SuperbSurvivorsContinued.NoPresetSpawn;
+NpcSpawnChance = SandboxVars.SuperbSurvivorsContinued.NpcSpawnChance;
+HostileSpawnRateBase = SandboxVars.SuperbSurvivorsContinued.HostileSpawnRateBase;
+HostileSpawnRateMax = SandboxVars.SuperbSurvivorsContinued.HostileSpawnRateMax;
+
+-- Raiders, Always hostile
+Limit_Raiders_Groups = SandboxVars.SuperbSurvivorsContinued.Limit_Raiders_Groups;
+Limit_Raiders_Spawn = SandboxVars.SuperbSurvivorsContinued.Limit_Raiders_Spawn;
+RaidersSpawnChance = SandboxVars.SuperbSurvivorsContinued.RaidersSpawnChance;
+RaidersSpawnFrequencyByHours = SandboxVars.SuperbSurvivorsContinued.RaidersSpawnFrequencyByHours;
+RaidersStartAfterHours = SandboxVars.SuperbSurvivorsContinued.RaidersStartAfterHours;
+
+-- NPC Configuration
+CanIdleChat = SandboxVars.SuperbSurvivorsContinued.CanIdleChat;
+CanNpcsCreateBase = SandboxVars.SuperbSurvivorsContinued.CanNpcsCreateBase;
+IsInfiniteAmmoEnabled = SandboxVars.SuperbSurvivorsContinued.IsInfiniteAmmoEnabled;
+IsRoleplayEnabled = SandboxVars.SuperbSurvivorsContinued.IsRoleplayEnabled;
+IsSpeakEnabled = SandboxVars.SuperbSurvivorsContinued.IsSpeakEnabled;
+SurvivorCanFindWork = SandboxVars.SuperbSurvivorsContinued.SurvivorCanFindWork;
+SurvivorNeedsFoodWater = SandboxVars.SuperbSurvivorsContinued.SurvivorNeedsFoodWater;
+SurvivorBravery = SandboxVars.SuperbSurvivorsContinued.SurvivorBravery;
+SurvivorFriendliness = SandboxVars.SuperbSurvivorsContinued.SurvivorFriendliness;
+SleepGeneralHealRate = SandboxVars.SuperbSurvivorsContinued.SleepGeneralHealRate;
+GFollowDistance = SandboxVars.SuperbSurvivorsContinued.GFollowDistance;
+PanicDistance = SandboxVars.SuperbSurvivorsContinued.PanicDistance;
+WepSpawnRateGun = SandboxVars.SuperbSurvivorsContinued.WepSpawnRateGun;
+WepSpawnRateMelee = SandboxVars.SuperbSurvivorsContinued.WepSpawnRateMelee;
+
+-- Player Related
+IsPlayerBaseSafe = SandboxVars.SuperbSurvivorsContinued.IsPlayerBaseSafe;
+IsPVPEnabled = SandboxVars.SuperbSurvivorsContinued.IsPVPEnabled;
+
+-- UI Related
+IsDisplayingNpcName = SandboxVars.SuperbSurvivorsContinued.IsDisplayingNpcName;
+IsDisplayingHostileColor = SandboxVars.SuperbSurvivorsContinued.IsDisplayingHostileColor;
