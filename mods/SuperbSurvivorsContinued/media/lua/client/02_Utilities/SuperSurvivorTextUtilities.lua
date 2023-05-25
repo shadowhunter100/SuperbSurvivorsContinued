bd14fde6b866b3490157cee07cbe4a3d0d085334
shadowhunter100 <shadowhunter100@users.noreply.github.com>
2023-05-25 14:35:25 -0700

    Updated Code Comments

--- mods/SuperbSurvivorsContinued/media/lua/client/02_Utilities/SuperSurvivorTextUtilities.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/02_Utilities/SuperSurvivorTextUtilities.lua
@@ -5,15 +5,14 @@ function Get_SS_UIActionText(text)
   return getText("IGUI_SS_" .. text)
 end
 
---- WIP - Cows: This is a terrible function name... the return value doesn't really seem to make sense if it is looking at Moodles...
---- WIP - Cows: Also, "getName()" is also the name of a core function... so this should be replaced or renamed when possible.
+--- WIP - Cows: Formerly "getName()", which has been renamed to "Get_SS_Name" because "getName() is also the name of a core function... 
 ---@param name any
 ---@return unknown
 function Get_SS_Name(name)
   return getText("Moodles_SS_" .. name)
 end
 
--- WIP - Cows: Get_SS_ContextMenuText() is a globl function... should consider updating the casing to reflect that.
+-- WIP - Cows: Frankly, I don't udnerstand why "ContextMenus_SS_" prefix is needed.
 ---@param text any
 ---@return unknown
 function Get_SS_ContextMenuText(text)
