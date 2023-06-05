9a1dead1df3de3b1c409225b5903f12a9454ceb5
shadowhunter100 <131904770+shadowhunter100@users.noreply.github.com>
2023-06-04 20:51:47 -0700

    Added an example for checking mods, credit to albion#0123 for sharing.

---
+++ mods/SuperbSurvivorsContinued/media/lua/client/00_SuperbSurviorModVariables/0_ModPatchingExamples.lua
@@ -0,0 +1,9 @@
+-- Cows: Thanks to albion#0123 for sharing this function.
+-- This will retrieve a list of ALL the active mods the user currently has.
+local activatedMods = getActivatedMods();
+
+-- Cows: Check if active mod lists includes <ModId> and write for handler for dealing with it.
+-- This block may need to be added to relevant functions such as equipment, perks, traits.
+if activatedMods:contains("ModId") then
+
+end
