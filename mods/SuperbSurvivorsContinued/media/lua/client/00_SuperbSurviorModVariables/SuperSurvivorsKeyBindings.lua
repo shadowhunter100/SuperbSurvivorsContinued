a491f927a3115a8857b84ad9216bb555abfde1de
shadowhunter100 <131904770+shadowhunter100@users.noreply.github.com>
2023-06-08 10:58:21 -0700

    Commented out KeyBindings file; the keys are now set and checked directly in the HotKeys file

--- mods/SuperbSurvivorsContinued/media/lua/client/00_SuperbSurviorModVariables/SuperSurvivorsKeyBindings.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/00_SuperbSurviorModVariables/SuperSurvivorsKeyBindings.lua
@@ -4,41 +4,43 @@
     Does the key override? How do we create a category in keys.ini?
     Maybe look into ModOptions to handle the key bindings?
 --]]
-local isLocalLoggingEnabled = false;
+
+-- 06-08-2023 - Cows: Commented out, don't actually need all these insertions if keynum can be set and matched in SuperSurvivorsModHotkeys
+-- local isLocalLoggingEnabled = false;
 
 -- WIP - Cows: The keybinding needs to be reworked.
 -- Using the global keyBinding table, to store all our binding values
-local function insertKeyBinding()
-	CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "function: insertKeyBinding() called");
-	local index = nil -- index will be the position we want to insert into the table
+-- local function insertKeyBinding()
+-- 	CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "function: insertKeyBinding() called");
+-- 	local index = nil -- index will be the position we want to insert into the table
 
-	for i, b in ipairs(keyBinding) do
-		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "index: " .. tostring(i));
-		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "key: " .. tostring(b.key));
-		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "value: " .. tostring(b.value));
-		-- "Shout" is the NPC interaction section in key.ini
-		if b.value == "Shout" then
-			index = i -- found the index, set it and break from the loop
-			break;
-		end
-	end
-	-- WIP - Cows: The hotkeys bindings are hard-coded here... it replaces all previous keybinds that were created perviously.
-	if index then
-		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "Inserting keyBinding ...");
-		-- we got a index, first lets insert our new entries
-		table.insert(keyBinding, index + 1, { value = "Call Closest Group Member", key = 55 });     -- Used to be key "T", updated to "numpad *" button
-		table.insert(keyBinding, index + 2, { value = "Call Closest Non-Group Member", key = 181 }); -- Used to be key "Y", updated to "numpad /" button
-		table.insert(keyBinding, index + 3, { value = "Ask Closest Group Member to Follow", key = 209 }); -- Used to be key "G", updated to "Page Down" button
-		table.insert(keyBinding, index + 4, { value = "Toggle Group Window", key = 201 });          -- Used to be key "Backspace", updated to "Page Up" button
-		table.insert(keyBinding, index + 5, { value = "Spawn Wild Survivor", key = 156 });          -- Used to be key "6", updated to "numpad Enter" button
-		table.insert(keyBinding, index + 6, { value = "Lower Follow Distance", key = 74 });         -- "numpad -"
-		table.insert(keyBinding, index + 7, { value = "Raise Follow Distance", key = 78 });         -- "numpad +"
-		table.insert(keyBinding, index + 8, { value = "SSHotkey_1", key = 200 });                   -- Up Arrow key
-		table.insert(keyBinding, index + 9, { value = "SSHotkey_2", key = 208 });                   -- Down Arrow key
-		table.insert(keyBinding, index + 10, { value = "SSHotkey_3", key = 203 });                  -- Left Arrow key
-		table.insert(keyBinding, index + 11, { value = "SSHotkey_4", key = 205 });                  -- Right Arrow key
-		table.insert(keyBinding, index + 12, { value = "NumPad_5", key = 76 });
-	end
-end
+-- 	for i, b in ipairs(keyBinding) do
+-- 		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "index: " .. tostring(i));
+-- 		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "key: " .. tostring(b.key));
+-- 		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "value: " .. tostring(b.value));
+-- 		-- "Shout" is the NPC interaction section in key.ini
+-- 		if b.value == "Shout" then
+-- 			index = i -- found the index, set it and break from the loop
+-- 			break;
+-- 		end
+-- 	end
+-- 	-- WIP - Cows: The hotkeys bindings are hard-coded here... it replaces all previous keybinds that were created perviously.
+-- 	if index then
+-- 		CreateLogLine("SuperSurvivorsKeyBindings", isLocalLoggingEnabled, "Inserting keyBinding ...");
+-- 		-- we got a index, first lets insert our new entries
+-- 		table.insert(keyBinding, index + 1, { value = "Call Closest Group Member", key = 55 });     -- Used to be key "T", updated to "numpad *" button
+-- 		table.insert(keyBinding, index + 2, { value = "Call Closest Non-Group Member", key = 181 }); -- Used to be key "Y", updated to "numpad /" button
+-- 		table.insert(keyBinding, index + 3, { value = "Ask Closest Group Member to Follow", key = 209 }); -- Used to be key "G", updated to "Page Down" button
+-- 		table.insert(keyBinding, index + 4, { value = "Toggle Group Window", key = 201 });          -- Used to be key "Backspace", updated to "Page Up" button
+-- 		table.insert(keyBinding, index + 5, { value = "Spawn Wild Survivor", key = 156 });          -- Used to be key "6", updated to "numpad Enter" button
+-- 		table.insert(keyBinding, index + 6, { value = "Lower Follow Distance", key = 74 });         -- "numpad -"
+-- 		table.insert(keyBinding, index + 7, { value = "Raise Follow Distance", key = 78 });         -- "numpad +"
+-- 		table.insert(keyBinding, index + 8, { value = "SSHotkey_1", key = 200 });                   -- Up Arrow key
+-- 		table.insert(keyBinding, index + 9, { value = "SSHotkey_2", key = 208 });                   -- Down Arrow key
+-- 		table.insert(keyBinding, index + 10, { value = "SSHotkey_3", key = 203 });                  -- Left Arrow key
+-- 		table.insert(keyBinding, index + 11, { value = "SSHotkey_4", key = 205 });                  -- Right Arrow key
+-- 		table.insert(keyBinding, index + 12, { value = "NumPad_5", key = 76 });
+-- 	end
+-- end
 
-insertKeyBinding()
+-- insertKeyBinding()
--- mods/SuperbSurvivorsContinued/media/lua/client/04_DataManagement/SuperSurvivorsModHotkeys.lua
+++ mods/SuperbSurvivorsContinued/media/lua/client/04_DataManagement/SuperSurvivorsModHotkeys.lua
@@ -3,6 +3,7 @@ local isLocalLoggingEnabled = false;
 -- WIP - Cows: This was made so the 4 arrow keys can be used to call the assigned orders in SuperSurvivorKeyBindAction() ...
 local function superSurvivorsHotKeyOrder(index)
     local order, isListening;
+    --
     if (index <= #Orders) then
         order = Orders[index];
         isListening = false;
@@ -10,9 +11,12 @@ local function superSurvivorsHotKeyOrder(index)
         order = Orders[(index - #Orders)];
         isListening = true;
     end
-    local myGroup = SSM:Get(0):getGroup()
+
+    local myGroup = SSM:Get(0):getGroup();
+    --
     if (myGroup) then
         local myMembers = myGroup:getMembersInRange(SSM:Get(0):Get(), 25, isListening);
+        --
         for i = 1, #myMembers do
             SurvivorOrder(nil, myMembers[i].player, order, nil)
         end
@@ -24,16 +28,13 @@ function SuperSurvivorKeyBindAction(keyNum)
     local isLocalFunctionLoggingEnabled = false;
     CreateLogLine("SuperSurvivorsHotKeys", isLocalFunctionLoggingEnabled, "function: SuperSurvivorKeyBindAction called");
     local playerSurvivor = getSpecificPlayer(0);
-
-    if (playerSurvivor) then
-        -- playerSurvivor:Say(tostring(keyNum));
-
-        if (keyNum == getCore():getKey("Spawn Wild Survivor")) then -- the NumPad enter key
+    --
+    if (playerSurvivor and playerSurvivor:isAlive()) then
+        --
+        if (keyNum == 56) then -- the NumPad enter key
             local activeNpcs = Get_SS_Alive_Count();
+            --
             if (activeNpcs < Limit_Npcs_Spawn) then
-                CreateLogLine("SuperSurvivorsHotKeys", isLocalFunctionLoggingEnabled, "Spawning NPC");
-                CreateLogLine("SuperSurvivorsHotKeys", isLocalFunctionLoggingEnabled, "activeNpcs: " .. tostring(activeNpcs));
-                CreateLogLine("SuperSurvivorsHotKeys", isLocalFunctionLoggingEnabled, "Limit_Npcs_Spawn: " .. tostring(Limit_Npcs_Spawn));
                 local ss = SuperSurvivorSpawnNpcAtSquare(playerSurvivor:getCurrentSquare());
                 -- Check if superb survivor spawned successfully
                 if (ss) then
@@ -42,71 +43,76 @@ function SuperSurvivorKeyBindAction(keyNum)
                     ss:setName("Spawned " .. name);
                 end
             else
-                CreateLogLine("SuperSurvivorsHotKeys", isLocalFunctionLoggingEnabled, "activeNpcs limit reached, no spawn.");
+                playerSurvivor:Say("activeNpcs limit reached, no spawn.");
             end
-        elseif (keyNum == getCore():getKey("Raise Follow Distance")) then
+        elseif (keyNum == 78) then  -- "numpad +" key
             if (GFollowDistance < 50) then
                 GFollowDistance = GFollowDistance + 1;
             end
             playerSurvivor:Say("Spread out more(" .. tostring(GFollowDistance) .. ")");
-        elseif (keyNum == getCore():getKey("Lower Follow Distance")) then
+        elseif (keyNum == 74) then -- "numpad -" key
             if (GFollowDistance > 0) then
                 GFollowDistance = GFollowDistance - 1;
             end
             playerSurvivor:Say("Stay closer(" .. tostring(GFollowDistance) .. ")");
-        elseif (keyNum == getCore():getKey("Call Closest Non-Group Member")) then
+        elseif (keyNum == 181) then --  "numpad /" key
             local mySS = SSM:Get(0);
             local SS = SSM:GetClosestNonParty();
-
+            --
             if (SS) then
                 mySS:Speak(Get_SS_Dialogue("HeyYou"))
-                SS:getTaskManager():AddToTop(ListenTask:new(SS, mySS:Get(), false))
+                SS:getTaskManager():AddToTop(ListenTask:new(SS, mySS:Get(), false));
             end
-        elseif (keyNum == getCore():getKey("Toggle Group Window")) then
-            window_super_survivors_visibility()
-        elseif (keyNum == getCore():getKey("Ask Closest Group Member to Follow")) then
-            local mySS = SSM:Get(0)
+        elseif (keyNum == 201) then -- "Page Up" key
+            window_super_survivors_visibility();
+        elseif (keyNum == 209) then -- "Page Down" key
+            local mySS = SSM:Get(0);
+            --
             if (mySS:getGroupID() ~= nil) then
-                local myGroup = SSGM:GetGroupById(mySS:getGroupID())
+                local myGroup = SSGM:GetGroupById(mySS:getGroupID());
+                --
                 if (myGroup ~= nil) then
-                    local member = myGroup:getClosestMember(nil, mySS:Get())
+                    local member = myGroup:getClosestMember(nil, mySS:Get());
+                    --
                     if (member) then
                         mySS:Get():Say(Get_SS_UIActionText("ComeWithMe_Before") ..
-                            member:Get():getForname() .. Get_SS_UIActionText("ComeWithMe_After"))
-                        member:getTaskManager():clear()
-                        member:getTaskManager():AddToTop(FollowTask:new(member, mySS:Get()))
+                            member:Get():getForname() .. Get_SS_UIActionText("ComeWithMe_After")
+                        );
+                        member:getTaskManager():clear();
+                        member:getTaskManager():AddToTop(FollowTask:new(member, mySS:Get()));
                     else
-                        CreateLogLine("SuperSurvivorsMod", isLocalFunctionLoggingEnabled, "getClosestMember returned nil");
+                        playerSurvivor:Say("getClosestMember returned nil");
                     end
                 else
-                    CreateLogLine("SuperSurvivorsMod", isLocalFunctionLoggingEnabled, "no group for player found");
+                    playerSurvivor:Say("no group for player found");
                 end
             end
-        elseif (keyNum == getCore():getKey("Call Closest Group Member")) then -- t key
-            local mySS = SSM:Get(0)
+        elseif (keyNum == 55) then -- "numpad *" key
+            local mySS = SSM:Get(0);
+            --
             if (mySS:getGroupID() ~= nil) then
-                local myGroup = SSGM:GetGroupById(mySS:getGroupID())
+                local myGroup = SSGM:GetGroupById(mySS:getGroupID());
+                --
                 if (myGroup ~= nil) then
-                    local member = myGroup:getClosestMember(nil, mySS:Get())
+                    local member = myGroup:getClosestMember(nil, mySS:Get());
+                    --
                     if (member) then
-                        mySS:Get():Say(member:Get():getForname() .. ", come here.")
-                        member:getTaskManager():AddToTop(ListenTask:new(member, mySS:Get(), false))
+                        mySS:Get():Say(member:Get():getForname() .. ", come here.");
+                        member:getTaskManager():AddToTop(ListenTask:new(member, mySS:Get(), false));
                     else
-                        CreateLogLine("SuperSurvivorsMod", isLocalFunctionLoggingEnabled, "getClosestMember returned nil");
+                        playerSurvivor:Say("getClosestMember returned nil");
                     end
-                else
-                    CreateLogLine("SuperSurvivorsMod", isLocalFunctionLoggingEnabled, "no group for player found");
                 end
             end
-        elseif (keyNum == getCore():getKey("SSHotkey_1")) then -- Up key, Order "Follow"
+        elseif (keyNum == 200) then -- Up key, Order "Follow"
             superSurvivorsHotKeyOrder(6);
-        elseif (keyNum == getCore():getKey("SSHotkey_2")) then -- Down key, Order "Stop"
+        elseif (keyNum == 208) then -- Down key, Order "Stop"
             superSurvivorsHotKeyOrder(19);
-        elseif (keyNum == getCore():getKey("SSHotkey_3")) then -- Left key, Order "Stand Ground"
+        elseif (keyNum == 203) then -- Left key, Order "Stand Ground"
             superSurvivorsHotKeyOrder(18);
-        elseif (keyNum == getCore():getKey("SSHotkey_4")) then -- Right key, Order "Barricade"
+        elseif (keyNum == 205) then -- Right key, Order "Barricade"
             superSurvivorsHotKeyOrder(1);
-        elseif (keyNum == getCore():getKey("NumPad_5")) then
+        elseif (keyNum == 76) then -- Numpad 5 key, log debug info.
             LogSSDebugInfo();
         end
     end
